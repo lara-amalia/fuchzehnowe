f68e5d0d70622abf8384696a6532bb58e65935b2
Lara Amalia <lara.aigmueller@gmail.com>
2020-04-03 14:27:09 +0200

    Fix firebase import

--- src/components/GameState/GameView/GameOver/GameOver.tsx
+++ src/components/GameState/GameView/GameOver/GameOver.tsx
@@ -1,4 +1,4 @@
-import * as firebase from 'firebase'
+import * as firebase from 'firebase/app'
 import React from 'react'
 import BasicLayout from '../../../ui/BasicLayout'
 import useGame from '../../../../util/useGame'
